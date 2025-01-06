478f81dfdbb7d30997443e4ef6d4b37c04672ded
Mila <milda.slepaviciute@gmail.com>
2025-01-06 02:29:34 +0200

    bootstrap problems fixing

--- peteandwolf-sveltekit/src/routes/+layout.svelte
+++ peteandwolf-sveltekit/src/routes/+layout.svelte
@@ -23,6 +23,14 @@
           transition: opacity 0.2s;
       }
   </style>
+
+  <!-- Load Bootstrap CSS before your custom styles -->
+  <link 
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+  rel="stylesheet" 
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+  crossorigin="anonymous"
+>
   
   <!-- Pre-load your fonts -->
   <link 
