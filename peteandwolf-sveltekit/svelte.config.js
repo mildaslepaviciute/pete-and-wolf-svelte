0c177221b578390a6b39f6289bd8d909bc1bef59
Mila <milda.slepaviciute@gmail.com>
2025-01-31 14:58:26 +0200

    specific runtime for config

--- peteandwolf-sveltekit/svelte.config.js
+++ peteandwolf-sveltekit/svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x'
+      runtime: 'nodejs18'  // Changed from 'nodejs18.x' to 'nodejs18'
     })
   },
   preprocess: vitePreprocess()
