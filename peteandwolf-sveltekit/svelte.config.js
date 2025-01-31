a545311d1a72f208d0c80014da96bb791568e3a9
Mila <milda.slepaviciute@gmail.com>
2025-01-31 14:52:09 +0200

    changing adapter, and vercel config

--- peteandwolf-sveltekit/svelte.config.js
+++ peteandwolf-sveltekit/svelte.config.js
@@ -1,15 +1,12 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';  // Changed this line
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     adapter: adapter({
-      fallback: 'index.html' // This ensures that all routes are handled by the SPA
-    }),
-    prerender: {
-      entries: ['*'] // Prerender all routes
-    }
+      runtime: 'nodejs18.x'
+    })
   },
   preprocess: vitePreprocess()
 };
