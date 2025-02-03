3701de63b6c766894e8a8b0434aaaf1f2451d4c0
Mila <milda.slepaviciute@gmail.com>
2025-02-03 22:03:15 +0200

    experiment with caching

--- peteandwolf-sveltekit/src/lib/sanityClient.js
+++ peteandwolf-sveltekit/src/lib/sanityClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: '46eerxjm',
   dataset: 'production',
-  apiVersion: '2024-01-31', // using today's date
+  apiVersion: '2024-02-03', // using today's date
   useCdn: false,
   ignoreBrowserTokenWarning: true,
   perspective: 'published'
