07bc013260f014d7b3c0484192e5e78c550bc201
Mila <milda.slepaviciute@gmail.com>
2025-04-17 09:28:29 +0300

    spacing for sonic-id main page as well

--- peteandwolf-sveltekit/src/routes/sonic-id/+page.svelte
+++ peteandwolf-sveltekit/src/routes/sonic-id/+page.svelte
@@ -23,28 +23,31 @@
     <meta name="description" content="Creating unique sonic identities">
 </svelte:head>
 
-{#each sonicIdData.sections as section, index}
-    <div id={section.title.replace(/\s+/g, '-').toLowerCase()} class="mb-4 mb-last-0">
-        {#if index === 0}
-            <h1 class="font-2 mb-3 fw-bold {getTitleAlignmentClass(section.titleAlignment)}">{section.title}</h1>
-        {:else}
-            <h2 class="font-2 mb-3 fw-bold {getTitleAlignmentClass(section.titleAlignment)}">{section.title}</h2>
+{#each sonicIdData.sections as section, sectionIndex}
+<div id={section.title.replace(/\s+/g, '-').toLowerCase()} class=" mb-last-0">
+    {#if sectionIndex === 0}
+        <h1 class="font-2  fw-bold {getTitleAlignmentClass(section.titleAlignment)}">{section.title}</h1>
+    {:else}
+        <h2 class="font-2  fw-bold {getTitleAlignmentClass(section.titleAlignment)}">{section.title}</h2>
+    {/if}
+    <hr class="my-4">
+    {#each section.blocks as block, blockIndex}
+        <div class="row gy-3 gy-lg-0 ">
+            {#if block.grid === 1}
+                <Column columnData={block.col_1} grid={block.grid} />
+            {:else if block.grid === 2}
+                <Column columnData={block.col_1} grid={block.grid} />
+                <Column columnData={block.col_2} grid={block.grid} />
+            {:else if block.grid === 3}
+                <Column columnData={block.col_1} grid={block.grid} />
+                <Column columnData={block.col_2} grid={block.grid} />
+                <Column columnData={block.col_3} grid={block.grid} />
+            {/if}
+        </div>
+
+        {#if !(sectionIndex === sonicIdData.sections.length - 1 && blockIndex === section.blocks.length - 1)}
+            <hr class="my-4">
         {/if}
-        <hr class="my-3">
-        {#each section.blocks as block}
-            <div class="row gy-3 gy-lg-0 mb-4">
-                {#if block.grid === 1}
-                    <Column columnData={block.col_1} grid={block.grid} />
-                {:else if block.grid === 2}
-                    <Column columnData={block.col_1} grid={block.grid} />
-                    <Column columnData={block.col_2} grid={block.grid} />
-                {:else if block.grid === 3}
-                    <Column columnData={block.col_1} grid={block.grid} />
-                    <Column columnData={block.col_2} grid={block.grid} />
-                    <Column columnData={block.col_3} grid={block.grid} />
-                {/if}
-            </div>
-            <hr class="my-3">
-        {/each}
-    </div>
+    {/each}
+</div>
 {/each}
