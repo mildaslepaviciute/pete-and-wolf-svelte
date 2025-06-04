7aea3ca80c2182cdd8f876abcf01285c3a633ad9
Mila <milda.slepaviciute@gmail.com>
2025-06-04 14:37:16 +0300

    adding arrows

--- peteandwolf-sveltekit/src/routes/+page.svelte
+++ peteandwolf-sveltekit/src/routes/+page.svelte
@@ -54,12 +54,33 @@
             {@html renderBlocks(homeData.description, true)}
           </div>
           <hr class="mt-2 mb-0">
-          <a href="/advertising" class="pill-link font-2 fw-bold text-black">Advertising</a>
+          <a href="/advertising" class="pill-link font-2 fw-bold text-black">
+            Advertising 
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right ms-1 pulse-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
+          </a>
           <hr class="mt-1 mb-1">
-          <a href="/cases/sonic-id" class="pill-link font-2 fw-bold  text-black">Cases</a>
+          <a href="/cases/sonic-id" class="pill-link font-2 fw-bold  text-black">
+            Cases
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right ms-1 pulse-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </a>
           <hr class="mt-1 mb-0">
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<style>
+  @keyframes pulse-right {
+    0% { transform: translateX(0); }
+    50% { transform: translateX(5px); }
+    100% { transform: translateX(0); }
+  }
+
+  .pulse-right {
+    animation: pulse-right 1.5s infinite ease-in-out;
+  }
+</style>
