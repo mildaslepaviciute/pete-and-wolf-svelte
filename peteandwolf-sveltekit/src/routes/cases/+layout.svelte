2ac9943f8727ca8521d59facb3537511da65f519
Mila <milda.slepaviciute@gmail.com>
2025-06-17 11:20:31 +0300

    fixing overflow in cases

--- peteandwolf-sveltekit/src/routes/cases/+layout.svelte
+++ peteandwolf-sveltekit/src/routes/cases/+layout.svelte
@@ -204,7 +204,7 @@
 
             <!-- Main content area -->
             <div class="col-lg-8 h-100 min-h-100 ps-lg-2 pe-10px-mob">
-               <div class="max-h-screen min-h-mob-screen h-100 scrolling border-lg border-black px-lg-3 py-lg-3">
+               <div class="max-h-screen min-h-mob-screen h-100 scrolling border-lg border-black px-lg-3 py-lg-3 overflow-x-hidden">
                     <div>
                         <slot />
                     </div>
