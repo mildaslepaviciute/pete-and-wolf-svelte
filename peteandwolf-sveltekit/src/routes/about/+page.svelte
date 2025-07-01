82529330cb63a62ec24428242c498c03e70666f7
Mila <milda.slepaviciute@gmail.com>
2025-07-01 09:50:46 +0300

    about page img size

--- peteandwolf-sveltekit/src/routes/about/+page.svelte
+++ peteandwolf-sveltekit/src/routes/about/+page.svelte
@@ -36,12 +36,12 @@
 <section class="h-100vh pt-below-nav overflow-hidden">
   <div class="container h-100 d-flex flex-column">
     <div class="row align-items-stretch scrolling-mob gy-4 gy-lg-0 pb-4 pb-lg-0" id="aboutRow">
-      <div class="col-lg-6 h-lg-100 d-flex flex-column">
+      <div class="col-lg-7 h-lg-100 d-flex flex-column">
         <div class="w-100">
           <img id="about-image" src={aboutData.mainImage.url} alt={aboutData.mainImage.alt || ""} class="w-100"/>
         </div>
       </div>
-      <div class="col-lg-6 h-lg-100 d-flex flex-column scrolling-lg">
+      <div class="col-lg-5 h-lg-100 d-flex flex-column scrolling-lg">
         <hr class="mt-0 mb-2 mb-lg-0">
         <h1 id="about-title" class="w-100 sticky-top bg-white font-2 font-3-mob mb-0 pb-0">
           <b>{aboutData.title}</b>
@@ -55,19 +55,19 @@
         <hr class="mb-2 mt-0 d-none d-lg-block">
         <div class="row g-0">
           <hr class="my-3 d-lg-none">
-          <div class="col-lg-4 d-flex flex-column  justify-content-end font-6-mob">
+          <div class="col-lg-6 d-flex flex-column  justify-content-end font-6-mob">
               {@html renderBlocks(aboutData.col_1, true, true)}
           </div>
           <hr class="my-3 d-lg-none">
-          <div class="col-lg-3 d-flex flex-column  justify-content-end font-6-mob">
+          <div class="col-lg-6 d-flex flex-column  justify-content-end font-6-mob">
               {@html renderBlocks(aboutData.col_2, true, true)}
           </div>
-          <hr class="my-3 d-lg-none">
-          <div class="col-lg-3 d-flex flex-column  justify-content-end font-6-mob">
+          <hr class="my-3 my-lg-2">
+          <div class="col-lg-6 d-flex flex-column  justify-content-end font-6-mob">
               {@html renderBlocks(aboutData.col_3, true, true)}
           </div>
           <hr class="my-3 d-lg-none">
-          <div class="col-lg-2 d-flex flex-column  justify-content-end font-6-mob">
+          <div class="col-lg-6 d-flex flex-column  justify-content-end font-6-mob">
               {@html renderBlocks(aboutData.col_4, true, true)}
           </div>
           <hr class="my-3 d-lg-none">
